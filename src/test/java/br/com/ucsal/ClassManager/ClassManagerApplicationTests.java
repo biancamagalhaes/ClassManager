622f39f7d3f023707d5fcaa03695b069e4973c41
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2020-04-28 17:29:39 -0300

    Project with an error when run it

---
+++ src/test/java/br/com/ucsal/ClassManager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucsal.ClassManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
